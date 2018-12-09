21283ed8a9a11c5b9abcc068f4e8fcafd16e3855
Judy Gong <jy2gong@uwaterloo.ca>
2018-12-09 19:46:39 +0100

    test1_judys_graphs

--- js/load_plotly.js
+++ js/load_plotly.js
@@ -5,6 +5,7 @@ $(document).ready(function(e) {
     // 'DALY_per_anxiety_continent',
     'prevalence_gender_disorder_share_global',
     'prevalence_gender_share_per_country',
+    'mental_illness_life_satisfaction'
     ]
     graphs.forEach(function(graph) {
       $('#' + graph).load('./plotly/' + graph + '.html',function(){console.log('loaded ' + graph)});
