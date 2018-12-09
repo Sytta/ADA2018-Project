48d865c43a6dc82f99cb5ab02d46825917646f20
Xinyi Cheng <xinyicheng@tsf-476-wpa-6-147.epfl.ch>
2018-12-09 17:13:34 +0100

    update js

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,7 @@
-// Show more Show less Button
+
 $(document).ready(function () {
     $('.more1').show();
     $('.less1').hide();
-    $('.more2').show();
-    $('.less2').hide();
-    $('.more3').show();
-    $('.less3').hide();
-    $('.more4').show();
-    $('.less4').hide();
-
     $('.more-button1').click(function () {
         $('.less1').show();
         $('.more-button1').hide();
@@ -18,7 +11,11 @@ $(document).ready(function () {
         $('.less-button1').hide();
         $('.less1').hide(300);
     });
+});
 
+$(document).ready(function () {
+    $('.more2').show();
+    $('.less2').hide();
     $('.more-button2').click(function () {
         $('.less2').show();
         $('.more-button2').hide();
@@ -28,17 +25,11 @@ $(document).ready(function () {
         $('.less-button2').hide();
         $('.less2').hide(300);
     });
+});
 
-    $('.more-button3').click(function () {
-        $('.less3').show();
-        $('.more-button3').hide();
-    });
-    $('.less-button3').click(function () {
-        $('.more3').show();
-        $('.less-button3').hide();
-        $('.less3').hide(300);
-    });
-
+$(document).ready(function () {
+    $('.more3').show();
+    $('.less3').hide();
     $('.more-button3').click(function () {
         $('.less3').show();
         $('.more-button3').hide();
@@ -49,3 +40,21 @@ $(document).ready(function () {
         $('.less3').hide(300);
     });
 });
+
+$(document).ready(function () {
+    $('.more4').show();
+    $('.less4').hide();
+    $('.more-button4').click(function () {
+        $('.less4').show();
+        $('.more-button4').hide();
+    });
+    $('.less-button4').click(function () {
+        $('.more4').show();
+        $('.less-button4').hide();
+        $('.less4').hide(300);
+    });
+});
+
+
+
+
