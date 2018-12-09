b896af2d54e6a18c0068edcbb34714d67fcfc222
Xinyi Cheng <xinyicheng@tsf-476-wpa-6-147.epfl.ch>
2018-12-09 16:44:19 +0100

    add show more/less button

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+function openSection(evt, sectionName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(sectionName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Show more Show less Button
+$(document).ready(function () {
+    $('.more').show();
+    $('.less').hide();
+    $('.more-button').click(function () {
+        $('.less').show();
+        $('.more-button').hide();
+    });
+    $('.less-button').click(function () {
+        $('.more').show();
+        $('.less-button').hide();
+        $('.less').hide(300);
+    });
+});
