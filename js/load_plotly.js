d75a41ab716464f00c6f6bb6a1716bf8634300ae
Judy Gong <jy2gong@uwaterloo.ca>
2018-12-09 20:06:42 +0100

    world map test1

--- js/load_plotly.js
+++ js/load_plotly.js
@@ -5,7 +5,8 @@ $(document).ready(function(e) {
     // 'DALY_per_anxiety_continent',
     'prevalence_gender_disorder_share_global',
     'prevalence_gender_share_per_country',
-    'mental_illness_life_satisfaction'
+    'mental_illness_life_satisfaction',
+    'mental_illness_map_over_years'
     ]
     graphs.forEach(function(graph) {
       $('#' + graph).load('./plotly/' + graph + '.html',function(){console.log('loaded ' + graph)});
