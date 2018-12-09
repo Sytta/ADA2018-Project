84666125030edecc91f6b005353aa3ed8e4e5302
Yawen Hou <yawen.hou@polymtl.ca>
2018-12-09 17:18:12 -0500

    Testing only load on scroll

---
+++ js/loadplotlyvisible.js
@@ -0,0 +1,25 @@
+
+$(window).scroll(function(){
+    $('.plot').each(function(){
+    if(isScrolledIntoView($(this))){
+        // $(this).children('span').text('visible');
+        console.log("Scrolled to " + this.id);
+        $(this).load('./plotly/' + this.id + '.html',function(){console.log('loaded ' + this.id)});
+        $(this).removeClass("plot");
+        
+    }
+    });
+});
+
+function isScrolledIntoView(elem){
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
